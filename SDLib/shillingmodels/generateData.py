d5934ad04b0c158bace419418174bb08f80689b4
rehan39 <116318327+rehan39@users.noreply.github.com>
2023-04-04 19:23:13 -0400

    Added shillingMethods
    Credit to https://github.com/Coder-Yu/SDLib for attackMethods

---
+++ SDLib/shillingmodels/generateData.py
@@ -0,0 +1,12 @@
+from averageAttack import AverageAttack
+from bandwagonAttack import BandWagonAttack
+from randomAttack import RandomAttack
+from RR_Attack import RR_Attack
+from hybridAttack import HybridAttack
+
+attack =AverageAttack('./config/config.conf')
+attack.insertSpam()
+#attack.farmLink()
+attack.generateLabels('labels.txt')
+attack.generateProfiles('profiles.txt')
+#attack.generateSocialConnections('relations.txt')
